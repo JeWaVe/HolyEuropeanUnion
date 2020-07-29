817000ad4ed716c2c4614251e035080b1ad98e33
JeWaVe <regis.portalez@altimesh.com>
2020-07-29 12:29:47 +0200

    impot et taxes + fix sur les accents (que javascript gère très mal)

--- content_script.js
+++ content_script.js
@@ -21,84 +21,95 @@ function handleText(textNode) {
 
 function replaceText(v)
 {
-    v = v.replace(/\bL'Union européenne\b/gi, "La Sainte Eglise Catholique");
-    v = v.replace(/\bUnion européenne\b/gi, "Sainte Eglise Catholique");
-    v = v.replace(/\bCommission européenne\b/gi, "Curie romaine");
-    v = v.replace(/\bl'Europe\b/gi, "L'Eglise");
-    v = v.replace(/\ble parlement européen\b/gi, "La Conférence Episcopale");
-    v = v.replace(/\bparlement européen\b/gi, "Conférence Episcopale");
-	v = v.replace(/\bdroit européen\b/gi, "droit canonique");
-	v = v.replace(/\btraités européens\b/gi, "droit canonique");
-	v = v.replace(/\btraités communautaires\b/gi, "droit canonique");
-    v = v.replace(/\beuropéen\b/gi, "très chrétien");
-    v = v.replace(/\beuropéens\b/gi, "très chrétiens");
-    v = v.replace(/\beuropéennes\b/gi, "très chrétiennes");
-    v = v.replace(/\beuropéenne\b/gi, "très chrétienne");
-    v = v.replace(/\bd'europe\b/gi, "de la chrétienté");
-    v = v.replace(/\bl'europe\b/gi, "la chrétienté");
-    v = v.replace(/\béconomique\b/gi, "Théologique");
-    v = v.replace(/\beurogroupe\b/gi, "Saint Empire Romain Germanique");
+    v = v.replace(/\bL'Union européenne\b/giu, "La Sainte Eglise Catholique");
+    v = v.replace(/\bUnion européenne\b/giu, "Sainte Eglise Catholique");
+    v = v.replace(/\bCommission européenne\b/giu, "Curie romaine");
+    v = v.replace(/\bl'Europe\b/giu, "L'Eglise");
+    v = v.replace(/\ble parlement européen\b/giu, "La Conférence Episcopale");
+    v = v.replace(/\bparlement européen\b/giu, "Conférence Episcopale");
+	v = v.replace(/\bdroit européen\b/giu, "droit canonique");
+	v = v.replace(/\btraités européens\b/giu, "droit canonique");
+	v = v.replace(/\btraités communautaires\b/giu, "droit canonique");
+    v = v.replace(/\beuropéen\b/giu, "très chrétien");
+    v = v.replace(/\beuropéens\b/giu, "très chrétiens");
+    v = v.replace(/\beuropéennes\b/giu, "très chrétiennes");
+    v = v.replace(/\beuropéenne\b/giu, "très chrétienne");
+    v = v.replace(/\bd'europe\b/giu, "de la chrétienté");
+    v = v.replace(/\bl'europe\b/giu, "la chrétienté");
+    v = v.replace(/\béconomique\b/giu, "Théologique");
+    v = v.replace(/\beurogroupe\b/giu, "Saint Empire Romain Germanique");
 	
 	// personnes
-    v = v.replace(/\bMario Draghi\b/gi, "Sa Sainteté le pape Mario");
-    v = v.replace(/\bChristine Lagarde\b/gi, "Sa Sainteté le pape Christine");
-    v = v.replace(/\bJean-Claude Juncker\b/gi, "Son Excellence Révérendissime Jean-Claude Juncker"); //José Manuel Durão Barroso
-    v = v.replace(/\bJosé Manuel Durão Barroso\b/gi, "Son Excellence Révérendissime José Manuel Durão Barroso"); //Ursula von der Leyen
-    v = v.replace(/\bUrsula von der Leyen\b/gi, "Son Excellence Révérendissime Ursula von der Leyen"); //Ursula von der Leyen
-    v = v.replace(/\bNicolas Sarkozy\b/gi, "Son Altesse Nicolas Sarkozy, Président très chrétien");
-    v = v.replace(/\bFrançois Hollande\b/gi, "Son Altesse François Hollande, Président très chrétien");
-    v = v.replace(/\bEmmanuel Macron\b/gi, "Son Altesse Emmanuel 1er, Président très chrétien");
-    v = v.replace(/\bMacron\b/gi, "Son Altesse Emmanuel 1er, Président très chrétien");
-    v = v.replace(/\bAngela Merkel\b/gi, "Sa Majesté Impériale Angela 1ere");
-    v = v.replace(/\bMerkel\b/gi, "Sa Majesté Impériale Angela 1ere");
-    v = v.replace(/\bRobert Schuman\b/gi, "Son Excellence Révérendissime Robert Schuman");
-    v = v.replace(/\bKonrad Adenauer\b/gi, "Son Excellence Révérendissime Konrad Adenauer");
-    v = v.replace(/\bJean Monnet\b/gi, "Son Excellence Révérendissime Jean Monnet");
-    v = v.replace(/\bAlcide de Gasperi\b/gi, "Son Excellence Révérendissime Alcide de Gasperi");
-    v = v.replace(/\bPaul-Henri Spaak\b/gi, "Son Excellence Révérendissime Paul-Henri Spaak");
-    v = v.replace(/\bJoseph Bech\b/gi, "Son Excellence Révérendissime Joseph Bech");
-    v = v.replace(/\bJohan Willem Beyen\b/gi, "Son Excellence Révérendissime Johan Willem Beyen");
-    v = v.replace(/\bJean Tirole\b/gi, "Le Très Révérend Père Jean Tirole");
-	v = v.replace(/\bFrédéric Lordon\b/gi, "Frédéric Lordon (hérétique au bûcher)");
-	v = v.replace(/\beurosceptique\b/gi, "hérétique");
-	v = v.replace(/\bEuroscepticisme\b/gi, "la très grave hérésie de l'euroscepticisme");
-	v = v.replace(/\bEurocritique\b/gi, "la grave hérésie de l'eurocritique");
-	v = v.replace(/\bEurocritiques\b/gi, "les hérésiques de l'eurocritique");
+    v = v.replace(/\bMario Draghi\b/giu, "Sa Sainteté le pape Mario");
+    v = v.replace(/\bChristine Lagarde\b/giu, "Sa Sainteté le pape Christine");
+    v = v.replace(/\bJean-Claude Juncker\b/giu, "Son Excellence Révérendissime Jean-Claude Juncker"); //José Manuel Durão Barroso
+    v = v.replace(/\bJosé Manuel Durão Barroso\b/giu, "Son Excellence Révérendissime José Manuel Durão Barroso"); //Ursula von der Leyen
+    v = v.replace(/\bUrsula von der Leyen\b/giu, "Son Excellence Révérendissime Ursula von der Leyen"); //Ursula von der Leyen
+    v = v.replace(/\bNicolas Sarkozy\b/giu, "Son Altesse Nicolas Sarkozy, Président très chrétien");
+    v = v.replace(/\bFrançois Hollande\b/giu, "Son Altesse François Hollande, Président très chrétien");
+    v = v.replace(/\bEmmanuel Macron\b/giu, "Son Altesse Emmanuel 1er, Président très chrétien");
+    v = v.replace(/\bMacron\b/giu, "Son Altesse Emmanuel 1er, Président très chrétien");
+    v = v.replace(/\bAngela Merkel\b/giu, "Sa Majesté Impériale Angela 1ere");
+    v = v.replace(/\bMerkel\b/giu, "Sa Majesté Impériale Angela 1ere");
+    v = v.replace(/\bRobert Schuman\b/giu, "Son Excellence Révérendissime Robert Schuman");
+    v = v.replace(/\bKonrad Adenauer\b/giu, "Son Excellence Révérendissime Konrad Adenauer");
+    v = v.replace(/\bJean Monnet\b/giu, "Son Excellence Révérendissime Jean Monnet");
+    v = v.replace(/\bAlcide de Gasperi\b/giu, "Son Excellence Révérendissime Alcide de Gasperi");
+    v = v.replace(/\bPaul-Henri Spaak\b/giu, "Son Excellence Révérendissime Paul-Henri Spaak");
+    v = v.replace(/\bJoseph Bech\b/giu, "Son Excellence Révérendissime Joseph Bech");
+    v = v.replace(/\bJohan Willem Beyen\b/giu, "Son Excellence Révérendissime Johan Willem Beyen");
+    v = v.replace(/\bJean Tirole\b/giu, "Le Très Révérend Père Jean Tirole");
+    v = v.replace(/\bJacques Attali\b/giu, "Le Très Révérend Père Jacques Attali");
+	v = v.replace(/\bFrédéric Lordon\b/giu, "Frédéric Lordon (hérétique au bûcher)");
+	v = v.replace(/\beurosceptique\b/giu, "hérétique");
+	v = v.replace(/\bEuroscepticisme\b/giu, "la très grave hérésie de l'euroscepticisme");
+	v = v.replace(/\bEurocritique\b/giu, "la grave hérésie de l'eurocritique");
+	v = v.replace(/\bEurocritiques\b/giu, "les hérésiques de l'eurocritique");
 	
 	// lieux
-    v = v.replace(/\bBruxelles\b/gi, "Saint-Siège");
-    v = v.replace(/\bFrancfort\b/gi, "Saint-Siège");
-    v = v.replace(/\bStrasbourg\b/gi, "Saint-Siège");
+    v = v.replace(/\bBruxelles\b/giu, "Saint-Siège");
+    v = v.replace(/\bFrancfort\b/giu, "Saint-Siège");
+    v = v.replace(/\bStrasbourg\b/giu, "Saint-Siège");
 	
 	// traités
-    v = v.replace(/\bTraité de Rome\b/gi, "Concile de Chalcédoine");
-    v = v.replace(/\bTraité de Maastricht\b/gi, "Concile de Trente");
-    v = v.replace(/\bde Maastricht\b/gi, "du concile de Trente");
-    v = v.replace(/\bTraité de Lisbonne\b/gi, "Concile de Vatican I");
-    v = v.replace(/\bTraité d'Amsterdam\b/gi, "Concile de Nicée");
-    v = v.replace(/\bTraité de Nice\b/gi, "Concile de Latran");
-    v = v.replace(/\btraité\b/gi, "Concile");
-    v = v.replace(/\btraités\b/gi, "Conciles");
-	
+    v = v.replace(/\bTraité de Rome\b/giu, "Concile de Chalcédoine");
+    v = v.replace(/\bTraité de Maastricht\b/giu, "Concile de Trente");
+    v = v.replace(/\bde Maastricht\b/giu, "du concile de Trente");
+    v = v.replace(/\bTraité de Lisbonne\b/giu, "Concile de Vatican I");
+    v = v.replace(/\bTraité d'Amsterdam\b/giu, "Concile de Nicée");
+    v = v.replace(/\bTraité de Nice\b/giu, "Concile de Latran");
 	
 	// théorie
-    v = v.replace(/\béconomiques\b/gi, "théologiques");
-    v = v.replace(/\béconomiste\b/gi, "théologien");
-    v = v.replace(/\béconomistes\b/gi, "théologiens");
-    v = v.replace(/\béconomique\b/gi, "théologique");
-    v = v.replace(/\bl'économie\b/gi, "la théologie");
-    v = v.replace(/\béconomie\b/gi, "théologie");
-    v = v.replace(/\bbrexit\b/gi, "Grand Schisme");
-	v = v.replace(/\blibéralisme\b/gi, "mystère très saint du libéralisme");
-	v = v.replace(/\bcapitalisme\b/gi, "mystère très saint du capitalisme");
-	v = v.replace(/\bruissellement\b/gi, "mystère très saint du ruissellement");
-	v = v.replace(/\blibre-échange\b/gi, "mystère très saint du libre-échange");
-	v = v.replace(/\bconcurrence\b/gi, "mystère très saint de la concurrence");
-	v = v.replace(/\bhétérodoxe\b/gi, "hérétique");
-	v = v.replace(/\journaliste\b/gi, "prêtre");
+    v = v.replace("économiste", "théologien");
+    v = v.replace("Économiste", "Théologien");
+    v = v.replace("économistes", "théologiens");
+    v = v.replace("économique", "théologique");
+    v = v.replace("économiques", "théologiques");
+    v = v.replace("l'économie", "la théologie");
+    v = v.replace("économie", "théologie");
+    v = v.replace("Économie", "Théologie");
+    v = v.replace(/\bbrexit\b/giu, "Grand Schisme");
+    v = v.replace(/\bfiscalité\b/giu, "Fiscalité punitive et confiscatoire");
+    v = v.replace("impôts", "impôts punitifs et confiscatoires");
+    v = v.replace("impôt", "impôt punitif et confiscatoire");
+    v = v.replace(/\bcharges sociales\b/giu, "charges sociales punitives et confiscatoires");
+    v = v.replace(/\bprestations sociales\b/giu, "prestations sociales de l'assistanat");
+    v = v.replace(/\bcotisations sociales\b/giu, "cotisations sociales punitives et confiscatoires");
+    v = v.replace(/\btaxes\b/giu, "taxes punitives et confiscatoires");
+    v = v.replace(/\bcapital\b/giu, "très saint capital");
+    v = v.replace(/\bcapitaux\b/giu, "très saints capitaux");
+	v = v.replace(/\blibéralisme\b/giu, "mystère très saint du libéralisme");
+	v = v.replace(/\bcapitalisme\b/giu, "mystère très saint du capitalisme");
+	v = v.replace(/\bruissellement\b/giu, "mystère très saint du ruissellement");
+	v = v.replace(/\blibre-échange\b/giu, "mystère très saint du libre-échange");
+	v = v.replace(/\bconcurrence\b/giu, "mystère très saint de la concurrence");
+	v = v.replace("hétérodoxe", "hérétique");
+	v = v.replace("hétérodoxes", "hérétiques");
+	v = v.replace(/\bjournaliste\b/giu, "prêtre");
+	v = v.replace("éditorialiste", "évêque");
 	
 	// inquisition
-	v = v.replace(/\btroïka\b/gi, "inquisition");
+	v = v.replace(/\btroïka\b/giu, "inquisition");
 
     return v;
 }
